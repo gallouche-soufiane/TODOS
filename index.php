df88368a83ad286c63d56f08e757f584a97b7a62
gallouche <soufiane.gallouche@hightech.edu>
2021-06-14 12:39:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+const ERROR_REQUIRED = 'Veuillez renseigner une todo';
+const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
+$filename = __DIR__ . "/data/todo.json";
+$error = '';
+$todo = '';
+$todos = [];
+
+if (file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $todos = json_decode($data, true) ?? [];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, [
+
+        "todo" => [
+            "filter" => FILTER_SANITIZE_STRING,
+            "flags" => FILTER_FLAG_NO_ENCODE_QUOTES | FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_BACKTICK
+        ]
+    ]);
+
+    $todo = $_POST['todo'] ?? '';
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } elseif (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+
+    if (!$error) {
+
+        $todos = [...$todos, [
+
+            'name' => $todo,
+            'done' => false,
+            'id' => time()
+
+        ]];
+
+        file_put_contents($filename, json_encode($todos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        $todo = '';
+        header('Location: /');
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once 'includes/head.php'; ?>
+    <title>Todo</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php'; ?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <form class="todo-form" action="/" method="post">
+                    <input value="<?= $todo ?>" name="todo" type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
+                <?php if ($error) : ?>
+                    <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
+                <ul class='todo-list'>
+                    <?php foreach ($todos as $t) : ?>
+                        <li class="todo-item <?= $t['done'] ? 'low-opacity' : '' ?>">
+                            <span class="todo-name"><?= $t['name'] ?></span>
+                            <a href="/edit-todo.php?id=<?= $t['id'] ?>">
+                                <button class="btn btn-primary btn-small"><?= $t['done'] ? 'Annuler' : 'Valider' ?></button>
+                            </a>
+                            <a href="/remove-todo.php?id=<?= $t['id'] ?>">
+                                <button class="btn btn-danger btn-small">Supprimer </button>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php'; ?>
+    </div>
+</body>
+
+</html>
---
+++ remove-todo.php
@@ -0,0 +1,16 @@
+<?php
+
+$filename = __DIR__ . "/data/todo.json";
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$id = $_GET['id'] ?? '';
+
+if ($id) {
+    $todos = json_encode(file_get_contents($filename), true) ?? [];
+    if (count($todos)) {
+        $todoIndex = array_search($id, array_column($todos, 'id'));
+        array_splice($todos, $todoIndex, 1);
+        file_put_contents($filename, json_encode($todos));
+    }
+}
+
+header('location: /');
